900695d742c3d72195f01ab02e294feadae44735
Aman Singh <98482324+AmanDoesntCode@users.noreply.github.com>
2022-11-22 14:34:56 +0530

    Added test Assert Values to the Test Case

--- src/com/lab9/TestAddNumbers.java
+++ src/com/lab9/TestAddNumbers.java
@@ -6,5 +6,9 @@ public class TestAddNumbers extends Object {
 
     @org.junit.Test
     public void sum() {
+        assertEquals(4, JunitTesting.Sum(2, 2));
+        assertEquals(0, JunitTesting.Sum(2, -2));
+        assertEquals(-4, JunitTesting.Sum(-2, -2));
+        assertEquals(1, JunitTesting.Sum(1, 0));
     }
 }
